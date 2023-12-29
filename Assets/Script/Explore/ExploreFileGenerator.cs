4fe1b084a269e864294774a85c5dae3242969ace
TRIIS\angel_yeh <a710594@gmail.com>
2023-12-29 17:39:10 +0800

    no message

--- Assets/Script/Explore/ExploreFileGenerator.cs
+++ Assets/Script/Explore/ExploreFileGenerator.cs
@@ -32,5 +32,31 @@ namespace Explore
             ExploreFile file = new ExploreFile(_info);
             DataContext.Instance.Save(file, Application.streamingAssetsPath + "./Map/Floor_1", DataContext.PrePathEnum.None);
         }
+
+        public void LoadFile() 
+        {
+            ExploreFile file = DataContext.Instance.Load<ExploreFile>(Application.streamingAssetsPath + "./Map/Floor_1", DataContext.PrePathEnum.Save);
+            for (int i = Tilemap.childCount; i > 0; --i)
+            {
+                DestroyImmediate(Tilemap.GetChild(0).gameObject);
+            }
+
+            GameObject obj;
+            for(int i=0; i<file.CellValues.Count; i++)
+            {
+                if (file.CellValues[i].CellType == Generator2D.CellType.Wall)
+                {
+                    obj = (GameObject)GameObject.Instantiate(Resources.Load("Prefab/Explore/Wall"), Vector3.zero, Quaternion.identity);
+                    obj.name = "Wall";
+                }
+                else
+                {
+                    obj = (GameObject)GameObject.Instantiate(Resources.Load("Prefab/Explore/Ground"), Vector3.zero, Quaternion.identity);
+                    obj.name = "Ground";
+                }
+                obj.transform.position = new Vector3(file.CellKeys[i].x, 0, file.CellKeys[i].y);
+                obj.transform.SetParent(Tilemap);
+            }
+        }
     }
 }
